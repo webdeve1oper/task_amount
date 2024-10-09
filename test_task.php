fdae0c6a2f50a96dee05bdd88bd06ed7fd5fe7a2
webdeve1oper <53707631+webdeve1oper@users.noreply.github.com>
2024-10-10 00:22:36 +0400

    Create test_task.php

---
+++ test_task.php
@@ -0,0 +1,26 @@
+
+  public function deductBalance(float $amount) {
+        $amount = round($amount, 8);
+
+        if ($amount <= 0) {
+            return redirect()->back()->withErrors(['error' => 'сумма не может быть отрицательной или равна нулю']);
+        }
+
+        if ($amount > $this->balance) {
+            return redirect()->back()->withErrors(['error' => 'недостаточно баланса']);
+        }
+
+        $this->balance -= $amount;
+        return $this->balance;
+    }
+
+    public function addBalance(float $amount) {
+        $amount = round($amount, 8);
+
+        if ($amount <= 0) {
+            return redirect()->back()->withErrors(['error' => 'сумма не может быть отрицательной или равна нулю']);
+        }
+
+        $this->balance += $amount;
+        return $this->balance;
+    }
